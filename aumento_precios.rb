2089068eaef7c40fe6bbf9714631c0d90f69da73
Tamara Rubilar <ta.rubilar@gmail.com>
2020-07-04 00:08:23 -0400

    start solution to the price increase program

---
+++ aumento_precios.rb
@@ -0,0 +1,9 @@
+=begin 
+
+Crear el programa aumento_precios.rb.
+
+Dentro del programa crear un método llamado augment que recibe un arreglo y un multiplicador, y
+que como resultado de un arreglo con todos los valores aumentados.
+
+=end 
+
